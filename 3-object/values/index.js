8194e27b0d800f3bc49321d187d2f186c8e40f1c
Anson <1072476988@qq.com>
2020-09-21 22:26:57 +0800

    [lingzhen] finish object practice.

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source)
+    .map(item => parseInt(item, 10))
+    .reduce((total, element) => total + element);
 }
