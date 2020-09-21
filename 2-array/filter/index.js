77d33d06cc5a5721de07d4df30b0c280b2f1a5a0
Anson <1072476988@qq.com>
2020-09-21 20:51:31 +0800

    [lingzhen] finish Array practice.

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,20 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(item => item % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  let unitCollection = [];
+  function containOrNot(number) {
+    if (!unitCollection.includes(number)) {
+      unitCollection.push(number);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return collection.filter(item => containOrNot(item));
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,7 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const date = new Date().getDate();
+  const date2000 = new Date(2000, 1, 1).getDate();
+  const number = date - date2000;
+  return collection.find(element => element.age < number).name;
 }
