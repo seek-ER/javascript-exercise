73f3e04e59804e2203ebba2cfa87ee7b3f77e0c4
Anson <1072476988@qq.com>
2020-09-22 08:37:26 +0800

    [lingzhen] finish async practice.

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,13 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  return fetch(url).then(responce => {
+    if (responce.status === 200) {
+      return responce.json();
+    }
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return Promise.reject('error msg');
+  });
   // end -->
 }
 
--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,9 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const response = await fetch(url);
+  const body = await response.json();
+  document.writeln(body.name);
   // end -->
 }
 
